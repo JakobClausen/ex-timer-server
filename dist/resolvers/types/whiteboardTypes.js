1d62e1526838ffae81a29971e707b94f5d98825f
Jakob Clausen <jakob.fridmar@gmail.com>
2020-11-02 13:29:43 +0100

    fixed

--- dist/resolvers/types/whiteboardTypes.js
+++ dist/resolvers/types/whiteboardTypes.js
@@ -73,7 +73,7 @@ __decorate([
 __decorate([
     type_graphql_1.Field(() => WhiteboardInput),
     __metadata("design:type", WhiteboardInput)
-], DaysInput.prototype, "tuesday", void 0);
+], DaysInput.prototype, "Tuesday", void 0);
 __decorate([
     type_graphql_1.Field(() => WhiteboardInput),
     __metadata("design:type", WhiteboardInput)
